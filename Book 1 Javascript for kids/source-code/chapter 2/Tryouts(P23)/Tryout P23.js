af8bddf62629ef4d637bcdf53de464202c387848
James1oliveira <oliveira1james@gmail.com>
2025-07-08 16:31:22 +0200

    Add files via upload

---
+++ Book 1 Javascript for kids/source-code/chapter 2/Tryouts(P23)/Tryout P23.js
@@ -0,0 +1,7 @@
+var balloons = 100;      // Declare a variable 'balloons' and assign it the value 100
+balloons *= 2;           // Multiply the value of 'balloons' by 2 (same as: balloons = balloons * 2)
+// balloons is now 200
+
+var balloons = 100;     // Declare a variable 'balloons' and assign it the value 100
+balloons /= 4;          // Divide the value of 'balloons' by 4 (same as: balloons = balloons / 4)
+// balloons is now 25
