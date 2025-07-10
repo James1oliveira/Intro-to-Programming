43298fccc794f799f67f434b6577e15181d22ec8
James1oliveira <oliveira1james@gmail.com>
2025-07-10 09:22:57 +0200

    Add files via upload

---
+++ Book 1 Javascript for kids/source-code/chapter 3/PC(61)/tryoutprogramming.js
@@ -0,0 +1,42 @@
+console.log("---------------------------------------")
+var randomBodyParts = ["mouth", "Nose", "armpit"];
+var randomAdjectives = ["Smelly", "Boring", "Stupid"];
+var randomAnimals = ["donkeys", "dogs", "Monkeys", "Rat"];
+var randomAnimalsbodyparts = ["ass", "nose", "ear", "tail"];
+// Pick a random body part from the randomBodyParts array:
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+// Pick a random adjective from the randomAdjectives array:
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+// Pick a random word from the randomAnimal array:
+var randomAnimals = randomAnimals[Math.floor(Math.random() * 4)];
+//pick a random animal body part array:
+var randomAnimalsbodyparts = randomAnimalsbodyparts[Math.floor(Math.random() * 4)];
+// Join all the random strings into a sentence:
+var randomInsult = " Your " + randomBodyPart + " is more " + 
+randomAdjective + " than a " + randomAnimals + " " + randomAnimalsbodyparts;
+console.log(randomInsult);
+
+console.log("----------------------------------------")
+var randomBodyParts = ["mouth", "Nose", "armpit"];
+var randomAdjectives = ["Smelly", "Boring", "Stupid"];
+var randomAnimals = ["donkeys", "dogs", "Monkeys", "Rat"];
+var randomAnimalsbodyparts = ["ass", "nose", "ear", "tail"];
+// Pick a random body part from the randomBodyParts array:
+var randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+// Pick a random adjective from the randomAdjectives array:
+var randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+// Pick a random word from the randomAnimal array:
+var randomAnimals = randomAnimals[Math.floor(Math.random() * 4)];
+//pick a random animal body part array:
+var randomAnimalsbodyparts = randomAnimalsbodyparts[Math.floor(Math.random() * 4)];
+// Join all the random strings into a sentence:
+var randomInsult = " Your " + randomBodyPart + " is more " + 
+randomAdjective + " than a " + randomAnimals + " " + randomAnimalsbodyparts;
+console.log(randomInsult);
+
+console.log("----------------------------------------")
+
+
+var number =[3, 2 ,1];
+var comparison = number.join(" is bigger than ");
+console.log(comparison);
