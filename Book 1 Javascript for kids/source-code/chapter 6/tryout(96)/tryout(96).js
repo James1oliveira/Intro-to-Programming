196e3683fc045bb019e5fe8ca961d43f3088f107
James1oliveira <oliveira1james@gmail.com>
2025-07-11 14:28:32 +0200

    Add files via upload

---
+++ Book 1 Javascript for kids/source-code/chapter 6/tryout(96)/tryout(96).js
@@ -0,0 +1,18 @@
+
+
+var name = "James";
+
+if (name === "Bob") {
+console.log("Hello me");} else  
+
+{console.log("Hello stranger...")};
+
+var mom = "Jess";
+var father = "Jack";
+if (mom){
+    console.log("Hello " +mom)
+} else if (father){
+    console.log("Hello " + Jack)
+} else{
+    console.log("Hello Stranger")
+}
