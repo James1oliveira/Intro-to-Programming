a12cf8e1b7cb842d84663cf3fb133f91f9cfe56d
James1oliveira <oliveira1james@gmail.com>
2025-07-14 11:35:06 +0200

    Add files via upload

---
+++ Book 2 Javascript for kids for dummies/chapter1/practise.js
@@ -0,0 +1,11 @@
+// Subtracts 37 from 2000 and logs the result (1963)
+console.log(2000 - 37);
+
+// Multiplies 30 by 27 and logs the result (810)
+console.log(30 * 27);
+
+// Divides 120 by 20 and logs the result (6)
+console.log(120 / 20);
+
+// Concatenates strings and logs the message: "Your name is learning JavaScript!"
+console.log("Your name" + " " + "is learning JavaScript!");
